dd61ae90fece5b8c8ad369fe571c15f0e3769433
Rusel T. Tayong <ruseltayong@gmail.com>
2023-11-17 18:55:48 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.body
+const div = document.createElement("div");
+const spanHi = document.querySelector("#hi");
+const spanBye = document.querySelector("#bye");
+
+spanHi.style.backgroundColor = "red";
